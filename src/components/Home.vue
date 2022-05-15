618cfb8f6a3cf61c7593eb32ba62ba09ca018516
Hugo <hugoversiani@live.com>
2022-05-15 07:38:15 -0300

    me contrata 🥺

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,43 @@
 <template>
   <main>
+
     <form @submit.prevent="enviar">
          <label for="valor">Valor:</label>
-        <input v-model="dados.valor" id="valor" type="text" name="valor"/>
+        <input v-model="dados.valor_emprestimo" id="valor" type="number" name="valor"/>
         
         <label for="instituicao">Instituição:</label>
-         <select v-model="dados.instituicao" id="instituicao" name="instituicao">
-            <option v-for="(instituicoes, index) in (instituicoes)" :key="index" :value="instituicoes.chave">{{instituicoes.valor}}</option>
+         <select multiple v-model="dados.instituicoes" id="instituicao" name="instituicao">
+            <option v-for="(instituicao, index) in (instituicao)" :key="index" :value="instituicao.chave">{{instituicao.valor}}</option>
         </select>
          <label for="convenio">Convênio:</label>
-        <select v-model="dados.convenio" id="convenio" name="convenio">
-            <option v-for="(convenios, index) in (convenios)" :key="index" :value="convenios.chave">{{convenios.valor}}</option>
+        <select multiple v-model="dados.convenios" id="convenio" name="convenio">
+            <option v-for="(convenio, index) in (convenio)" :key="index" :value="convenio.chave">{{convenio.valor}}</option>
         </select>
          <label for="parcelas">Parcelas:</label>
-        <select v-model="dados.parcelas" name="parcelas">
-            <option value="36">36</option>
-            <option value="40">40</option>
-            <option value="60">60</option>
-            <option value="72">72</option>
-            <option value="84">84</option>
+        <select v-model="dados.parcela" name="parcelas">
+            <option value=36>36</option>
+            <option value=40>40</option>
+            <option value=60>60</option>
+            <option value=72>72</option>
+            <option value=84>84</option>
         </select>
         <button type="submit">Simular</button>
     </form>
+    
+    <div class="popup">
+      <div v-for="(simulacao, index) in (simulacao)" :key="index">
+        <h2>{{index}}</h2>
+        <span class="valor">R$ {{dados.valor_emprestimo}}</span>
+      
+          <div class="conteudo-simulacao" v-for="(simulacao, index) in (simulacao)" :key="index">
+            <span v-if="dados.parcela <= simulacao.parcelas">
+            {{simulacao.parcelas}} parcelas de R$ {{simulacao.valor_parcela}} <br>
+            </span>
+            <span v-if="dados.parcela <= simulacao.parcelas">Taxa de juros: {{simulacao.taxa}}</span>
+          </div>
+          
+      </div>
+    </div>
   </main>
 </template>
 <script>
@@ -33,33 +49,40 @@ export default {
 
   data() {
       return {
-          dados: {
-            valor_emprestimo: "",
-            instituicoes: [],
-            convenios: [],
-            parcela: ""
+          simulacao: [],
 
+          dados: {
+            valor_emprestimo: '',
+            instituicoes:[] ,
+            convenios: [],
+            parcela: '',
+      
           },
         
           parcelas: "",
-          instituicoes: [],
-          convenios: []
+          instituicao: [],
+          convenio: []
       }
   },
   mounted() {
       api.get('/instituicao').then(response => {
-          this.instituicoes = response.data;
+          this.instituicao = response.data;
       });
       api.get('/convenio').then(response => {
-          this.convenios = response.data;
+          this.convenio = response.data;
       })
   },
   methods: {
       enviar() {
-         api.post('/simular', this.dados).then(response => {
-           alert(response.data);
+        this.dados.parcela = parseInt(this.dados.parcela)
+        const dados  = this.dados
+         api.post('/simular', { valor_emprestimo: dados.valor_emprestimo,
+                                instituicoes: dados.instituicoes,
+                                convenios: dados.convenios,
+    parcela: 60}).then(response => {
+         this.simulacao = response.data;
+        
          })
-
       }
   }
 
@@ -71,6 +94,8 @@ main {
   height: calc(100vh - 95px);
   display: flex;
   justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 
 form {
@@ -83,7 +108,13 @@ form {
 
 
 }
+form label {
+    display: flex;
+    width: 50%;
+    align-items: flex-start;
+    padding-top: 10px;
 
+}
 
 form input, select {
     padding: 10px;
@@ -106,7 +137,69 @@ button {
     transition: 0.15s ease-in-out,
 }
 
+h2 {
+  color: var(--color-secondary);
+  font-size: 1.8em;
+  padding: 10px 0;
+}
+
+.valor {
+  color: rgb(167, 167, 167);
+  font-size: 1.6em;
+  font-weight: bold;
+}
+
 button:hover {
     background-color: #c45800;
 }
+
+.popup {
+  width: 50%;
+  height: 500px;
+  border:1px solid var(--color-primary);
+  border-radius: 10px;
+  margin-bottom: 50px;
+  overflow-y: scroll;
+  padding: 30px
+  
+}
+.conteudo-simulacao {
+  background-color: rgb(241, 241, 241);
+  padding: 10px 10px;
+  margin: 10px 0;
+  border-radius: 5px;
+}
+.conteudo-simulacao span {
+  margin: 12px 0;
+  color: rgb(56, 56, 56);
+ 
+}
+
+.conteudo-simulacao span:first-child {
+  font-weight: bold;
+  font-size: 1.4em;
+  margin-bottom: 10px;
+}
+
+.conteudo-simulacao span:last-child {
+  margin: 12px 0;
+}
+@media (max-width: 1024px) {
+  form input, select {
+    width: 90%;
+  }
+
+  .popup {
+    width: 90%;
+  }
+
+  form label {
+    display: flex;
+    width: 90%;
+    align-items: flex-start;
+    padding-top: 10px;
+
+}
+}
+
 </style>
